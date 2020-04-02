73cb28351c9396a7d324dbe7586205459e88075c
Aaron <southpawflash@hotmail.com>
2020-04-02 14:51:08 -0400

    updated app.js and refactored

--- backend-api/src/main/java/org/wcci/apimastery/HighResponse.java
+++ backend-api/src/main/java/org/wcci/apimastery/HighResponse.java
@@ -1,9 +1,15 @@
 package org.wcci.apimastery;
 
+import java.util.Collection;
+
 public class HighResponse {
     private String contactInfo;
 
-    public HighResponse String contactInfo) {
+    public HighResponse(String contactInfo) {
         this.contactInfo = contactInfo;
-}
+    }
+
+    public Collection<String> getContactInfo() {
+        return contactInfo;
+    }
 }
--- backend-api/src/main/java/org/wcci/apimastery/LowResponse.java
+++ backend-api/src/main/java/org/wcci/apimastery/LowResponse.java
@@ -1,18 +1,28 @@
 package org.wcci.apimastery;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class LowResponse {
-    private String instructions;
+    private HashMap<String, String> lowResponseInstructions = new HashMap<String, String >();
+    public LowResponse(){
+            lowResponseInstructions.put("anxious","go for a walk");
+            lowResponseInstructions.put("sad","deep breathing exercise");
+            lowResponseInstructions.put("depressed","call a loved one or friend");
+            lowResponseInstructions.put("angry","exercise");
+            lowResponseInstructions.put("overwhelmed","make list to organize your thoughts");
+            lowResponseInstructions.put("lonely","get a pet");
+            lowResponseInstructions.put("exhausted","take a nap");
 
-    public LowResponse(String instructions){
-        this.instructions = instructions;
+          }
 
-    }
-    public Collection<String> getInstructions(){
-        return instructions;
+//
+// create API controller
+
 
 }
 
 
-}
+
+
