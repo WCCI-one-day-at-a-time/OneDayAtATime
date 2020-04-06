51f3303addc6b81d4f24bba6e905fbade02d3c00
Aaron <southpawflash@hotmail.com>
2020-04-06 11:29:21 -0400

    first two controller tests pass

--- backend-api/src/main/java/org/wcci/apimastery/Controllers/LandingController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controllers/LandingController.java
@@ -1,19 +1,33 @@
 package org.wcci.apimastery.Controllers;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.wcci.apimastery.Model.UserSubmission;
 import org.wcci.apimastery.Repositories.UserSubmissionRepository;
 
+import java.util.Collection;
+
 @Controller
     public class LandingController {
     UserSubmissionRepository userSubmissionRepo;
 
-        @PostMapping("/submit")
+
+
+    @PostMapping("/submit")
         public UserSubmission addNewSubmission(@RequestBody UserSubmission submissionToAdd) {
             return userSubmissionRepo.save(submissionToAdd);
         }
 
+    @GetMapping("/submissions")
+    public Collection<UserSubmission> retrieveUserSubmission() {
+        return (Collection<UserSubmission>) userSubmissionRepo.findAll();
 
+    }
+
+    public LandingController(UserSubmissionRepository userSubmissionRepo) {
+        this.userSubmissionRepo = userSubmissionRepo;
+    }
 }
