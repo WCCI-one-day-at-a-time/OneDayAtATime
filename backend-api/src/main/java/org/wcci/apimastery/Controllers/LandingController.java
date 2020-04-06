7e04ab4fe9cb30e39c08aead00bdc31d54b63b70
Aaron <southpawflash@hotmail.com>
2020-04-06 11:52:07 -0400

    got http request test working

--- backend-api/src/main/java/org/wcci/apimastery/Controllers/LandingController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controllers/LandingController.java
@@ -5,29 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.wcci.apimastery.Model.UserSubmission;
 import org.wcci.apimastery.Repositories.UserSubmissionRepository;
 
 import java.util.Collection;
 
-@Controller
+@RestController
     public class LandingController {
-    UserSubmissionRepository userSubmissionRepo;
+        UserSubmissionRepository userSubmissionRepo;
 
 
 
-    @PostMapping("/submit")
-        public UserSubmission addNewSubmission(@RequestBody UserSubmission submissionToAdd) {
-            return userSubmissionRepo.save(submissionToAdd);
+@PostMapping("/submit")
+public UserSubmission addNewSubmission(@RequestBody UserSubmission submissionToAdd) {
+        return userSubmissionRepo.save(submissionToAdd);
         }
 
-    @GetMapping("/submissions")
-    public Collection<UserSubmission> retrieveUserSubmission() {
+@GetMapping("/submissions")
+public Collection<UserSubmission> retrieveUserSubmission() {
         return (Collection<UserSubmission>) userSubmissionRepo.findAll();
 
-    }
+        }
 
-    public LandingController(UserSubmissionRepository userSubmissionRepo) {
+public LandingController(UserSubmissionRepository userSubmissionRepo) {
         this.userSubmissionRepo = userSubmissionRepo;
-    }
-}
+        }
+        }
--- backend-api/src/test/java/org/wcci/apimastery/LandingControllerTest.java
+++ backend-api/src/test/java/org/wcci/apimastery/LandingControllerTest.java
@@ -43,6 +43,8 @@ public class LandingControllerTest {
         verify(userSubmissionRepository).findAll();
 
     }
+
+
 }
 
 
