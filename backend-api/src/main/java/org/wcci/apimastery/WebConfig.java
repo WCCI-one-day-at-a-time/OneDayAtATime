4edf39ab273467ee35c9515727aece39ed5397b4
ja-br <letstalk@jamesbrooks.tech>
2020-04-08 11:29:17 -0400

    Modified webconfig

--- backend-api/src/main/java/org/wcci/apimastery/WebConfig.java
+++ backend-api/src/main/java/org/wcci/apimastery/WebConfig.java
@@ -1,23 +1,19 @@
 package org.wcci.apimastery;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
+public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "PUT", "POST", "DELETE", "PATCH");
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
     }
