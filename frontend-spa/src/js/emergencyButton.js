68c2c456e29edaa21caa6b5eed3696fe586e3072
Nick Fada <nfada2010@gmail.com>
2020-04-08 14:10:11 -0400

    emergency button working

---
+++ frontend-spa/src/js/emergencyButton.js
@@ -0,0 +1,11 @@
+let urgent = document.querySelector(".urgent")
+
+const emergency = () =>{
+    console.log("emergencyPressed")
+    midRowTwo.innerHTML = `<div class="card__container"></div>`
+   
+   
+   
+}
+
+urgent.addEventListener("click", emergency)
