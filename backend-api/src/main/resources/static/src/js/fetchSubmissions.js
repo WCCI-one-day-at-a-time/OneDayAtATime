0cfd25573831ae92a52ded1e582d45fd258e3078
Benjamin Williams <ben@wecancodeit.org>
2020-04-20 12:04:54 -0400

    Readying for deployment

--- backend-api/src/main/resources/static/src/js/fetchSubmissions.js
+++
@@ -1,7 +0,0 @@
-const getSubmissions = () => {
-    fetch('http://localhost:8080/response')
-        .then(response => response.json())
-        .then(submissions => renderCalendarView(submissions))
-
-
-}
